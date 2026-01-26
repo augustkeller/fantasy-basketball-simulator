043117f5f0920f8e5ce02d6dd8a33e4f1b7455c1
augustkeller <guskeller@gmail.com>
2026-01-26 14:26:35 -0600

    Game Mode Selection page first draft added

---
+++ fantasy-basketball/src/pages/GameModeSelect.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
+export default function GameModeSelect() {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Fantasy Basketball Simulator</h1>
+
+      <div style={{ marginTop: "40px" }}>
+        <button onClick={() => navigate("/one-player")}>
+          1 Player Mode
+        </button>
+
+        {/* Future modes */}
+        <button disabled style={{ marginLeft: "16px" }}>
+          Multiplayer (Coming Soon)
+        </button>
+      </div>
+    </div>
+  );
+}
