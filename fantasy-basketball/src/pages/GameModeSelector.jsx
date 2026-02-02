6214f95b369e00b0f9b35b6337308fe549812dd6
augustkeller <guskeller@gmail.com>
2026-02-02 12:05:48 -0600

    GameModeSelector updated for multiple game modes

--- fantasy-basketball/src/pages/GameModeSelector.jsx
+++ fantasy-basketball/src/pages/GameModeSelector.jsx
@@ -1,19 +1,32 @@
 import { useNavigate } from "react-router-dom";
 
-export default function GameModeSelector() {
+export default function GameModeSelector({ setGameMode }) {
   const navigate = useNavigate();
+
+  function startSinglePlayer() {
+    setGameMode("single");
+    navigate("/select");
+  }
+
+  function startTwoPlayer() {
+    setGameMode("two-player");
+    navigate("/select");
+  }
 
   return (
     <div>
       <h1>Fantasy Basketball Simulator</h1>
 
       <div style={{ marginTop: "40px" }}>
-        <button onClick={() => navigate("/one-player")}>
+        <button onClick={startSinglePlayer}>
           1 Player Mode
         </button>
 
-        {/* Future modes */}
-        <button disabled style={{ marginLeft: "16px" }}>
+        <button
+          onClick={startTwoPlayer}
+          style={{ marginLeft: "16px" }}
+          disabled
+        >
           Multiplayer (Coming Soon)
         </button>
       </div>
