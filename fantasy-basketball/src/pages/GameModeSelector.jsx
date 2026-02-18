148e97403656f82b5d4249c25a6d98027e1661b6
augustkeller <guskeller@gmail.com>
2026-02-18 12:18:20 -0600

    GameModeSelector updated attempt to integrate UserCountSelector

--- fantasy-basketball/src/pages/GameModeSelector.jsx
+++ fantasy-basketball/src/pages/GameModeSelector.jsx
@@ -1,32 +1,35 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import UserCountSelector from "../components/UserCountSelector";
 
-export default function GameModeSelector({ setGameMode }) {
+export default function GameModeSelector() {
   const navigate = useNavigate();
   const [userCount, setUserCount] = useState(1);
 
-
-  function startSinglePlayer() {
-    setGameMode("single");
-    navigate("/select");
-  }
-
-  function startTwoPlayer() {
-    setGameMode("two-player");
-    navigate("/select");
+  function handleStartGame() {
+    navigate("/select", { state: { userCount } });
   }
 
   return (
     <div>
       <h1>Fantasy Basketball Simulator</h1>
 
-      <div style={{ marginTop: "40px", display: "flex", gap: "16px", justifyContent: "center" }}>
+      <div
+        style={{
+          marginTop: "40px",
+          display: "flex",
+          gap: "16px",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
         <UserCountSelector
           value={userCount}
           onChange={setUserCount}
         />
-        <Button onClick={() => navigate("/player-select", { state: { userCount } })}>
+
+        <Button onClick={handleStartGame}>
           Start Game
         </Button>
       </div>
