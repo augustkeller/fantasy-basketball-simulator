844cbbc96eabd231cde59987cd472710b2b4bd8f
augustkeller <guskeller@gmail.com>
2026-02-18 12:13:14 -0600

    UserCountSelector rewired -first steps

--- fantasy-basketball/src/pages/GameModeSelector.jsx
+++ fantasy-basketball/src/pages/GameModeSelector.jsx
@@ -1,8 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
+import UserCountSelector from "../components/UserCountSelector";
 
 export default function GameModeSelector({ setGameMode }) {
   const navigate = useNavigate();
+  const [userCount, setUserCount] = useState(1);
+
 
   function startSinglePlayer() {
     setGameMode("single");
@@ -19,12 +22,12 @@ export default function GameModeSelector({ setGameMode }) {
       <h1>Fantasy Basketball Simulator</h1>
 
       <div style={{ marginTop: "40px", display: "flex", gap: "16px", justifyContent: "center" }}>
-        <Button onClick={startSinglePlayer}>
-          1 Player Mode
-        </Button>
-
-        <Button onClick={startTwoPlayer}>
-          2 Player Mode
+        <UserCountSelector
+          value={userCount}
+          onChange={setUserCount}
+        />
+        <Button onClick={() => navigate("/player-select", { state: { userCount } })}>
+          Start Game
         </Button>
       </div>
     </div>
